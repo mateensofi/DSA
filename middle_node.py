1f34716c1c0577f2fc97014e20366e0ca981a77a
Mateen Sofi <mateensofi02@gmail.com>
2023-12-04 16:05:22 +0530

    Finding the Middle Node without the length

---
+++ middle_node.py
@@ -0,0 +1,51 @@
+"""
+Problem 1: Finding the middle node without the length attribute.
+If the linked list has an even number of nodes, return the first node of the second half of the list.
+The method should traverse the linked list once i.e., only one loop is allowed.
+"""
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+
+# Test for odd number of elements
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+print(my_linked_list.find_middle_node().value)
+
+# Test for even number of elements
+my_linked_list.append(6)
+
+print(my_linked_list.find_middle_node().value)
